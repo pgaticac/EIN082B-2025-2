0c4c6afb5a52bbeee380ca820b7884f3ff9ef4e9
pgaticac <pamela.gatica@usm.cl>
2025-09-02 10:44:08 -0400

    Semana 5 - JS Ejercicio Ojo

--- Semana5/js/sctript.js
+++ Semana5/js/sctript.js
@@ -1,6 +1,14 @@
+setInterval(picar,1000)
+
 function saludar(){
    // document.write("Hola desde función")
    var miH1 = document.getElementById("saludo")
    miH1.classList.add("rojo")
    miH1.innerHTML = "Hola desde función"
 }
+
+function picar(){
+   var ojo = document.getElementById("ojo")
+   ojo.classList.toggle("ojo-cerrado")
+}
+
