b9690680a2ea8b2c42d7f0a6d4f7813ebfbe6d10
pgaticac <pamela.gatica@usm.cl>
2025-10-14 10:50:31 -0300

    Semana 11 - Django Admin (user: admin / pwd: admin)

--- Semana11/core/admin.py
+++ Semana11/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Proveedor, Producto
 
-# Register your models here.
+
+admin.site.register(Producto)
+admin.site.register(Proveedor)
--- Semana11/core/models.py
+++ Semana11/core/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Proveedor(models.Model):
     rut = models.CharField(max_length=13, primary_key=True)
     nombre = models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name_plural = "Proovedores"
 
 class Producto(models.Model):
     codigo = models.CharField(max_length=6,primary_key=True)
